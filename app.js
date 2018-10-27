a11ce1e8f221e2aeada27281e00e0609e44b2868
iiiml7 <44521591+iiiml7@users.noreply.github.com>
2018-10-27 16:01:09 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+const {Client} = require('discord.js');
+const client = new Client();
+
+client.on('ready', () => {
+    setInterval(function(){
+        client.guilds.get('333278908357017612').roles.find('name', 'R').edit({color: 'RANDOM'}) 
+    },100);
+})
+
+client.login(process.env.BOT_TOKEN)
